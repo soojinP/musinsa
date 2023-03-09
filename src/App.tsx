d925edb0de89f5b7b96bc96ab2e018a6b619e26d
soojin_park <soojin_park@tmax.co.kr>
2023-03-09 17:47:30 +0900

    feat: query client, provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-// import './App.css'
+import React from 'react'
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil'
 import Page from './pages/Page'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+//TODO: focus out 됬다가 다시 돌아왔을 때 똑같은 query 또 날리는거 어떻게 방지해?
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            retry: false,
+        },
+    },
+})
 
 function App() {
-    return <Page />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <Page />
+            </RecoilRoot>
+        </QueryClientProvider>
+    )
 }
 
 export default App
